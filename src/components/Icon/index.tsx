b2332e67955dd710506a30435afec13cb1d9328b
usyuuryou <usyuuryou@163.com>
2023-04-01 12:45:40 +0800

    Update Icon

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Icons } from "../../constants/Icon";
+
+export const Icon: React.FC = () => {
+  return (
+    <img
+      src="/assets/img/bootstrap.svg"
+      alt="Bootstrap"
+      width="32"
+      height="32"
+    />
+  );
+};
+
+export default Icon;
